6f75ebd9453bb90ebb8daa85e318bc174b50c51e
John Howe <94157812+johnh-otu@users.noreply.github.com>
2021-11-29 15:22:16 -0500

    rough outline of layout implementation

--- product-page.php
+++ product-page.php
@@ -1,12 +1,35 @@
+<?php
+    #retrieve product info and store it in variables
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
-        <title></title>
+        <title><!DOCTYPE html>
+<html>
+    <head>
+        <title>DFJJ - </title> <!-- there might be a way to put product name here-->
+        <link rel="stylesheet" href="site-styles.css">
+    </head>
+    <body>
+        <div>
+            <!--Header bar here-->
+        </div>
+
+        <?php
+
+            
+
+
+        ?>
+
+    </body>
+</html></title>
         <link rel="stylesheet" href="site-styles.css">
     </head>
     <body>
         
-
+        <!-- some stuff here in html and php to display product info-->
 
 
     </body>
--- search-page.php
+++ search-page.php
@@ -1,13 +1,30 @@
+<?php
+    #connect to database and retrieve product info
+    #turn products into an array of product objects?
+    $products = []
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
-        <title></title>
+        <title>DFJJ - Our Products</title>
         <link rel="stylesheet" href="site-styles.css">
     </head>
     <body>
+        <div>
+            <!--Header bar here-->
+        </div>
+
+        <?php
+
+            for(int i = 0; i < count($products); i++)
+            {
+                #add td/div/whatever for product
+            }
 
 
 
+        ?>
 
     </body>
 </html>
