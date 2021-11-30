32999e6daf5eb28cbc99f337f8445ac2f55009a4
John Howe <94157812+johnh-otu@users.noreply.github.com>
2021-11-30 14:20:51 -0500

    Update product-page.php

--- product-page.php
+++ product-page.php
@@ -5,9 +5,6 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title><!DOCTYPE html>
-<html>
-    <head>
         <title>DFJJ - </title> <!-- there might be a way to put product name here-->
         <link rel="stylesheet" href="site-styles.css">
     </head>
