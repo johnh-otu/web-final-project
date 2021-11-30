1b27ca6577155c850418855fcb48a20b090d2441
John Howe <94157812+johnh-otu@users.noreply.github.com>
2021-11-30 14:58:44 -0500

    created navbar with centered brand

--- product-page.php
+++ product-page.php
@@ -6,12 +6,16 @@
 <html>
     <head>
         <title>DFJJ - </title> <!-- there might be a way to put product name here-->
+        <link href="/bootstrap-5.1.3-dist/css/bootstrap.min.css" rel="stylesheet">
         <link rel="stylesheet" href="site-styles.css">
     </head>
     <body>
         <nav class="navbar navbar-expand-md navbar-light bg-light mb-4">
             <div class="container-fluid">
-
+                <a class="navbar-brand" href="#">Home</a>
+                <span class="navbar-brand mx-auto">
+                    <h2 id="dfjj">DFJJ</h2>
+                </span>
             </div>
         </nav>
 
