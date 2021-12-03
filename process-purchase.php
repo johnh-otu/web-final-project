1ef2043105a08d51f0fb0b0decb7ee73dfce54f6
John Howe <94157812+johnh-otu@users.noreply.github.com>
2021-12-03 08:28:56 -0500

    final touches

--- process-purchase.php
+++ process-purchase.php
@@ -30,42 +30,50 @@
         echo '<script>location.replace("/search-page");</script>';
     }
 
-    define("connectionString","mysql:dbname=finalproject");
-    define("userName","root");
-    define("password","");
-
-    $pdo = new PDO(connectionString, userName, password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    $sql = 
-
-    $price = 0;
-    for($i = 0; $i < count($cartArr); $i++)
+    try
     {
-        $pid = $cartArr[$i][0];
-        $pprices = $pdo->query("SELECT price FROM products WHERE product_id = " . $pid . ";")->fetchAll()[0];
-        $price += $pprices[0];
+        define("connectionString","mysql:dbname=finalproject");
+        define("userName","root");
+        define("password","");
+
+        $pdo = new PDO(connectionString, userName, password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        $price = 0;
+        for($i = 0; $i < count($cartArr); $i++)
+        {
+            $pid = $cartArr[$i][0];
+            $pprices = $pdo->query("SELECT price FROM products WHERE product_id = " . $pid . ";")->fetchAll()[0];
+            $price += $pprices[0];
+        }
+
+        $sql = "INSERT INTO `information` (`first_name`, `last_name`, `street_address`, `city`, `postal`, `email`, `date`, `total_price`, `province`, `country`) 
+            VALUES (". $pdo->quote($first) .",". $pdo->quote($last) .",". $pdo->quote($address) .",". $pdo->quote($city) .",". $pdo->quote($postal) .",". $pdo->quote($email) .", (SELECT CAST(CURRENT_TIMESTAMP as DATE)),". $price .",". $pdo->quote($province) .",". $pdo->quote($country) .");";
+
+        $rows = $pdo->prepare($sql)->execute(); //insert
+        echo $rows;
+
+        $sql = "SELECT purchase_id FROM information WHERE email=" . $pdo->quote($email) . " AND total_price=" . $price . ";";
+        $purchase_id = $pdo->query($sql)->fetch();
+
+        
+        for ($i=0; $i < count($cartArr); $i++) 
+        {
+            $pid = $cartArr[$i][0];
+            $size = $cartArr[$i][1]; 
+            $sql = "INSERT INTO `purchases` (`purchase_id`, `product_id`, `size`) VALUES (" . $purchase_id . ", " . $pid . ", " . $size . ");";
+            $rows = $pdo->prepare($sql)->execute(); //insert
+            echo $rows;
+        }
+
+        $pdo = null;
     }
-
-    $sql = "INSERT INTO `information` (`first_name`, `last_name`, `street_address`, `city`, `postal`, `email`, `total_price`, `province`, `country`) 
-        VALUES (". $first .",". $last .",". $address .",". $city .",". $postal .",`". $email ."`,". $price .",". $province .",". $country .");";
-
-    $pdo->prepare($sql)->execute(); //insert
-
-    $sql = "SELECT purchase_id FROM information WHERE email=" . $pdo->quote($email) . " AND price=" . $price . ";";
-    $purchase_id = $pdo->query($sql)->fetch();
-
+    catch(PDOException $e)
+    {
+        echo $e->getMessage();
+    }
     
-    for ($i=0; $i < count($cartArr); $i++) 
-    {
-        $pid = $cartArr[$i][0];
-        $size = $cartArr[$i][1]; 
-        $sql = "INSERT INTO `purchases` (`purchase_id`, `product_id`, `size`) VALUES (" . $purchase_id . ", " . $pid . ", " . $size . ");";
-        $pdo->prepare($sql)->execute(); //insert
-    }
-
-    $pdo = null;
 
     setcookie('cart', "", time()-3600);
-    echo '<script>location.replace("/thankyou-page");</script>';
+    //echo '<script>location.replace("/thankyou-page");</script>';
 ?>
