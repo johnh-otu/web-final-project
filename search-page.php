45cc1fd8e473eb51f1ea0bed34c5194812ec4b33
John Howe <94157812+johnh-otu@users.noreply.github.com>
2021-12-02 23:27:36 -0500

    Update search-page.php

--- search-page.php
+++ search-page.php
@@ -11,45 +11,35 @@
         $pdo = new PDO(connectionString, userName, password);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        $condition = "1";
+        $condition = array();
+        $condition[0] = "1";
 
         echo "<script>console.log('yo');</script>";
         echo "<script>console.log('". isset($_GET['c']) ."');</script>";
 
         if(count($_GET) >= 1) //for filtering
         {
-            echo "<script>console.log('yo');</script>";
 
             if(isset($_GET['g']))
             {
                 $gender = $_GET['g'];
-                echo '<script>console.log("raw value: '. $condition . " AND gender = " . $pdo->quote($gender) . " OR 'Unisex'" .'");</script>';
-                
-                $condtition = $condition . " AND gender = " . $pdo->quote($gender) . " OR 'Unisex'";
-                echo '<script>console.log("condition value: '. $condition .'");</script>';
+                $condition[1] = " AND gender = " . $pdo->quote($gender) . " OR 'Unisex'";
             }
             if(isset($_GET['c']))
             {
                 $colour = $_GET['c'];
-                $condition = $condition . " AND color = " . $pdo->quote($colour);
+                $condition[2] = " AND color = " . $pdo->quote($colour);
             }
             if(isset($_GET['t']))
             {
                 $type = $_GET['t'];
-                $condition = $condition . " AND type = " . $pdo->quote($type);
+                $condition[3] = " AND type = " . $pdo->quote($type);
             }
-
-            echo '<script>console.log("'. $condition .'");</script>';
         }
         
-        echo '<script>console.log("'. $condition .'");</script>';
-        $sql = "SELECT * from products WHERE ". $condition .";";
+        $sql = "SELECT * FROM products WHERE ". $condition[0] . (isset($condition[1])?$condition[1]:""). (isset($condition[2])?$condition[2]:""). (isset($condition[3])?$condition[3]:"") .";";
+        echo '<script>console.log("sql value: '. $sql .'");</script>';
         $products = $pdo->query($sql)->fetchAll(); //returns as a 2-D array
-
-        /* or possibly
-        $statement = $conn->prepare($sql);
-        $statement->execute();
-        */
 
         $pdo = null; //disconnect
     }
@@ -119,114 +109,124 @@
         </nav>
 
         <!--main-->
-        <main class="">
+        
+        <div class="row">
+
+            <!---Divider-->
+
             <div class="row">
-
-                <!---Divider-->
-
-                <div class="row">
+                <form method="get" action="search-page.php">
                     <div class="flex-shrink-0 p-3 bg-white col-2" style="width: 280px;">
+                        <ul class="list-unstyled ps-0">
+                            <li class="mb-1">
+                            <!--<button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#gender-collapse" aria-expanded="false">
+                                Gender
+                            </button>-->
+                            <div class="ms-4"><h5>Gender</h5></div>
+                            <div class="col" id="gender-collapse">
+                                <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                                <li><input type="radio" name="g" value="Men" id="g-m" class="ms-4 me-3"><label for="g-m">Men</label></li>
+                                <li><input type="radio" name="g" value="Women" id="g-w" class="ms-4 me-3"><label for="g-w">Women</label></li>
+                                <li><input type="radio" name="g" value="Unisex" id="g-u" class="ms-4 me-3"><label for="g-u">Unisex</label></li>
+                                </ul>
+                            </div>
+                            </li>
+                            <li class="mb-1">
+                            <!--<button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#colour-collapse" aria-expanded="false">
+                                Colour
+                            </button>-->
+                            <div class="ms-4"><h5>Colour</h5></div>
+                            <div class="" id="colour-collapse">
+                                <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                                <li><input type="radio" name="c" value="Red" id="c-r" class="ms-4 me-3"><label for="c-r">Red</label></li>
+                                <li><input type="radio" name="c" value="Blue" id="c-b" class="ms-4 me-3"><label for="c-b">Blue</label></li>
+                                <li><input type="radio" name="c" value="Black" id="c-k" class="ms-4 me-3"><label for="c-k">Black</label></li>
+                                <li><input type="radio" name="c" value="Gray" id="c-a" class="ms-4 me-3"><label for="c-a">Gray</label></li>
+                                <li><input type="radio" name="c" value="Green" id="c-g" class="ms-4 me-3"><label for="c-g">Green</label></li>
+                                <li><input type="radio" name="c" value="White" id="c-w" class="ms-4 me-3"><label for="c-w">White</label></li>
+                                <li><input type="radio" name="c" value="Purple" id="c-p" class="ms-4 me-3"><label for="c-p">Purple</label></li>
+                                <li><input type="radio" name="c" value="Pink" id="c-n" class="ms-4 me-3"><label for="c-n">Pink</label></li>
+                                </ul>
+                            </div>
+                            </li>
+                            <li class="mb-1">
+                            <!--<button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#style-collapse" aria-expanded="false">
+                                Style
+                            </button>-->
+                            <div class="ms-4"><h5>Style</h5></div>
+                            <div class="" id="style-collapse">
+                                <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                                <li><input type="radio" name="t" value="Top" id="t-t" class="ms-4 me-3"><label for="t-t">Tops</label></li>
+                                <li><input type="radio" name="t" value="Bottom" id="t-b" class="ms-4 me-3"><label for="t-b">Bottoms</label></li>
+                                <li><input type="radio" name="t" value="Accessories" id="t-a" class="ms-4 me-3"><label for="t-a">Accessories</label></li>
+                                </ul>
+                            </div>
+                            </li>
+                            <li class="border-top my-3"></li>
+                            <li class="mb-1">
+                            <!--<button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#filter-collapse" aria-expanded="false">
+                                Filter
+                            </button>-->
+                            <div class="ms-4"><h5>Filter</h5></div>
+                            <div class="" id="filter-collapse">
+                                <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                                <li><a href="#" class="link-dark rounded">A-Z</a></li>
+                                <li><a href="#" class="link-dark rounded">Z-A</a></li>
+                                <li><a href="#" class="link-dark rounded">High-Low Price</a></li>
+                                <li><a href="#" class="link-dark rounded">Low-High Price</a></li>
+                                </ul>
+                            </div>
+                            </li>
+                            <li>
+                                <button type="submit" class="w-100 btn btn-primary mt-3">Search <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16"><path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/></svg></button>
+                            </li>
+                        </ul>
+                    </div>
+                </form>
+            <div class="b-example-divider"></div> 
+
+            <!---Pictures-->
+
+            <div class="album py-5 bg-light col">
+                <div class="container">
+                    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
                         
-                    <ul class="list-unstyled ps-0">
-                        <li class="mb-1">
-                        <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#home-collapse" aria-expanded="true">
-                            Men
-                        </button>
-                        <div class="collapse show col" id="home-collapse">
-                            <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-                            <li><a href="#" class="link-dark rounded">Shirt</a></li>
-                            <li><a href="#" class="link-dark rounded">Pants</a></li>
-                            <li><a href="#" class="link-dark rounded">Accesories</a></li>
-                            </ul>
-                        </div>
-                        </li>
-                        <li class="mb-1">
-                        <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#dashboard-collapse" aria-expanded="false">
-                            Women
-                        </button>
-                        <div class="collapse" id="dashboard-collapse">
-                            <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-                            <li><a href="#" class="link-dark rounded">Shirt</a></li>
-                            <li><a href="#" class="link-dark rounded">Pants</a></li>
-                            <li><a href="#" class="link-dark rounded">Accesories</a></li>
-                            </ul>
-                        </div>
-                        </li>
-                        <li class="mb-1">
-                        <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#orders-collapse" aria-expanded="false">
-                            Children
-                        </button>
-                        <div class="collapse" id="orders-collapse">
-                            <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-                            <li><a href="#" class="link-dark rounded">Shirt</a></li>
-                            <li><a href="#" class="link-dark rounded">Pants</a></li>
-                            <li><a href="#" class="link-dark rounded">Accesories</a></li>
-                            </ul>
-                        </div>
-                        </li>
-                        <li class="border-top my-3"></li>
-                        <li class="mb-1">
-                        <button class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#account-collapse" aria-expanded="false">
-                            Filter
-                        </button>
-                        <div class="collapse" id="account-collapse">
-                            <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-                            <li><a href="#" class="link-dark rounded">A-Z</a></li>
-                            <li><a href="#" class="link-dark rounded">Z-A</a></li>
-                            <li><a href="#" class="link-dark rounded">High-Low Price</a></li>
-                            <li><a href="#" class="link-dark rounded">Low-High Price</a></li>
-                            </ul>
-                        </div>
-                        </li>
-                    </ul>
-                </div>
+                        <?php
 
-                <div class="b-example-divider"></div> 
+                            for($i = 0; $i < count($products); $i++)
+                            {
+                                $pname = $products[$i]['product_name'];
+                                $pprice = $products[$i]['price'];
+                                $pimg = $products[$i]['image'];
+                                $pid = $products[$i]['product_id'];
 
-                <!---Pictures-->
+                                #<svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="'. $pimg .'" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
 
-                <div class="album py-5 bg-light col">
-                    <div class="container">
-                        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-                            
-                            <?php
-
-                                for($i = 0; $i < count($products); $i++)
-                                {
-                                    $pname = $products[$i]['product_name'];
-                                    $pprice = $products[$i]['price'];
-                                    $pimg = $products[$i]['image'];
-                                    $pid = $products[$i]['product_id'];
-
-                                    #<svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="'. $pimg .'" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-
-                                    echo '
-                                    <div class="col">
-                                        <div class="card shadow-sm">
-                                            <img src="data:image/jpeg;base64,'.base64_encode($pimg).'" width=200 height=200/> 
-                                            <div class="card-body">
-                                                <p>
-                                                    <a href="/product-page?pid='. $pid .'">
-                                                    <span>'. $pname .'</span>
-                                                    </a>
-                                                </p>
-                                                <div class="d-flex justify-content-between align-items-center">
-                                                    <small class="text-muted">$'. $pprice .'</small>
-                                                </div>
+                                echo '
+                                <div class="col">
+                                    <div class="card shadow-sm">
+                                        <img src="data:image/jpeg;base64,'.base64_encode($pimg).'" width=200 height=200 class="mx-auto"/> 
+                                        <div class="card-body">
+                                            <p style="text-align:center;">
+                                                <a href="/product-page?pid='. $pid .'">
+                                                <span>'. $pname .'</span>
+                                                </a>
+                                            </p>
+                                            <div class="d-flex justify-content-between align-items-center">
+                                                <small style="text-align:center;" class="text-muted">$'. $pprice .'</small>
                                             </div>
                                         </div>
-                                    </div>';
-                                    
-                                }
-                            ?>
-                            
-                        </div>
+                                    </div>
+                                </div>';
+                                
+                            }
+                        ?>
+                        
                     </div>
                 </div>
             </div>
+        </div>
 
-
-        </main>
 
         <footer class="text-muted py-5">
             <div class="container">
